ea26541319c0d9940e73ed272094551bacb421cd
alexaway <1527381991@qq.com>
2016-11-28 22:48:30 +0800

    Mon Nov 28 22:48:30 CST 2016

---
+++ private/alexaway/init.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+cd ~
+if [ -e .gitconfig ]
+then 
+    rm .gitconfig
+    git config --global user.name alexaway
+    git config --global user.email 1527381991@qq.com
+    git config --global core.editor eamcs
+    git config --global color.ui true
+    git config --global push.default simple
+fi
+
+if [ -d .emacs.d ]
+then
+   echo "the .emacs.d directory exists!"
+   mv .emacs.d .emacs.d.bak
+   git clone git@github.com:alexaway/spacemacs.git .emacs.d
+fi
+
+cp ~/.emacs.d/.spacemacs ~/.spacemacs
+
+cd ~/.emacs.d
+git remote add alexaway git@github.com:alexaway/spacemacs.git
