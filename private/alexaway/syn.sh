60edbe72c6d31ef855560b5007f2506f958b4495
alexaway <1527381991@qq.com>
2016-11-28 20:19:03 +0800

    Mon Nov 28 20:19:03 CST 2016

--- private/alexaway/syn.sh
+++ private/alexaway/syn.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-if [ -n $(git ls-files -m) ]
-then
+cd ~/.emacs.d/private/alexaway/
+#if [ -n $(git ls-files -m) ]
+#then
     git pull
     git add .
     git commit -m "$(date)"
     git push
-fi
+#fi
