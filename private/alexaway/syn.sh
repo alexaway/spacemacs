0a2c35c3cb8e81d0034457ba4903649152f69376
alexaway <1527381991@qq.com>
2016-11-29 11:19:09 +0800

    Tue Nov 29 11:19:09 CST 2016

--- private/alexaway/syn.sh
+++ private/alexaway/syn.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 cd ~/.emacs.d/private/alexaway/
-git pull alexaway/master
+git pull alexaway
 if [ "$(git ls-files -m)" = "" ]
 then
     echo "do nothing"
 else    
     git add .
     git commit -m "$(date)"
-    git push -u alexaway/master
+    git push -u alexaway
 fi
