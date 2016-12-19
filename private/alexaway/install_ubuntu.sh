b16d023708eb92f2ec3ebab202ffcfaf9671bb6f
alexaway <1527381991@qq.com>
2016-12-19 21:58:33 +0800

    Mon Dec 19 21:58:33 CST 2016

--- private/alexaway/install_ubuntu.sh
+++ private/alexaway/install_ubuntu.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-apt-get install unity-tweak-tool conky-all git clang build-essential cmake python-dev
+apt-get --assume-yes install unity-tweak-tool conky-all git clang build-essential cmake python-dev
 
 add-apt-repository ppa:noobslab/themes
 apt-get update
-apt-get install flatabulous-theme
+apt-get --assume-yes install flatabulous-theme
 
 add-apt-repository ppa:noobslab/icons
 apt-get update
 apt-get install ultra-flat-icons
 
-apt-get install zsh
+apt-get --assume-yes install zsh
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 chsh -s /bin/zsh
 
 add-apt-repository ppa:ubuntu-elisp/ppa
 apt-get update
-apt-get install emacs-snapshot
+apt-get --assume-yes install emacs-snapshot
