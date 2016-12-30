67cbacee591cd878a347de0944cb263b0cd3ccc2
alexaway <1527381991@qq.com>
2016-12-30 14:00:10 +0800

    2016年 12月 30日 星期五 14:00:09 CST

--- private/alexaway/packages.el
+++ private/alexaway/packages.el
@@ -770,6 +770,7 @@ same directory as the org-buffer and insert a link to this file."
               ))
       (setq calendar-holidays holiday-alexaway-holidays)
       (setq calendar-mark-holidays-flag t)
+
       )
 
     )
