df8ed7f5aa389f5bdc8d799db96b6b2359c2a78b
alexaway <1527381991@qq.com>
2017-01-07 11:02:23 +0800

    2017年 01月 07日 星期六 11:02:23 CST

--- private/alexaway/init.sh
+++ private/alexaway/init.sh
@@ -11,6 +11,11 @@ git config --global user.email 1527381991@qq.com
 git config --global core.editor eamcs
 git config --global color.ui true
 git config --global push.default simple
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset' --abbrev-commit --"
+git config --global alias.lga "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset' --abbrev-commit --all"
+git config --global alias.lg1 "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+git config --global alias.lg2 "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
+
 
 if [ -d .emacs.d ]
 then
