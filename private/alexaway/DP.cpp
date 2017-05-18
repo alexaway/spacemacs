79847f048858da47000422cc4ea5dabd8ef81efe
alexaway <nonearoundme@gmail.com>
2017-05-18 22:41:37 +0800

    Update DP.cpp

--- private/alexaway/DP.cpp
+++ private/alexaway/DP.cpp
@@ -705,6 +705,25 @@ int lengthOfLIS(vector<int>& nums) {
         }
         return len;
 }
+/*120. triangle
+Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
+
+For example, given the following triangle
+[
+     [2],
+    [3,4],
+   [6,5,7],
+  [4,1,8,3]
+]
+The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).*/
+int minimumTotal(vector<vector<int>>& triangle) {
+        for(int i=triangle.size()-2;i>=0;--i){
+            for(int j=0;j<=i;++j){
+                triangle[i][j]+=min(triangle[i+1][j+1],triangle[i+1][j]);
+            }
+        }
+        return triangle[0][0];
+}
 int main()
 {
   vector<int> v;
